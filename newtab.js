1ea0aa726bc7a873fb05063176ef215ed01a2def
Kevin Li <kl2010@ohs.stanford.edu>
2024-09-10 21:14:41 -0700

    Delete newtab.js

--- newtab.js
+++
@@ -1,7 +0,0 @@
-chrome.storage.sync.get('apodImage', (data) => {
-    if (data.apodImage) {
-        document.body.style.backgroundImage = `url(${data.apodImage})`;
-    } else {
-        document.body.style.backgroundColor = '#000'; // Fallback color if the image is not available
-    }
-});
